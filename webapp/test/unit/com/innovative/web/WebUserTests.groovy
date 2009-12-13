dde92d6defa9ddb9af0830ee504f59c2d90c5a8a
cristian <cristian@4ac4832c-dd6c-11dc-94f1-d760ba12e90c>
2009-12-13 19:23:40 +0000

    Initial commit

---
+++ webapp/test/unit/com/innovative/web/WebUserTests.groovy
@@ -0,0 +1,17 @@
+package com.innovative.web
+
+import grails.test.*
+
+class WebUserTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
