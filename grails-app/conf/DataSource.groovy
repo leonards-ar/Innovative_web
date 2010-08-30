5adcf0e44009291d70d82b6eead852121237d1d6
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2010-08-30 11:02:16 +0000

    Adding autoreconnect parameter

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,7 +15,7 @@ environments {
 		dataSource {
                     // dbCreate = "create-drop" // one of 'create', 'create-drop','update'
                     dbCreate = 'update'
-                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8"
+                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8&autoReconnect=true"
 		}
                 hibernate {
                     show_sql=true
@@ -24,13 +24,13 @@ environments {
 	test {
 		dataSource {
                     dbCreate = "update"
-                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8"
+                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8&autoReconnect=true"
 		}
 	}
 	production {
 		dataSource {
                     //dbCreate = "update"
-                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8"
+                    url = "jdbc:mysql://localhost/innovative_web_db?useUnicode=true&characterEncoding=utf-8&autoReconnect=true"
 		}
         }
 }
